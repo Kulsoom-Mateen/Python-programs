c672be1edee55e044b2cfa78824cdbba3dce515a
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-03-25 17:37:41 -0400

    Added four pyrhon programs ALCULATOR.py LOWERCASE_UPPERCASE.py PRIME_NUMBER.py and SALARY.py

---
+++ lowercase_uppercase.py
@@ -0,0 +1,5 @@
+a="Pakistan is great"
+print(a.upper())
+print(a.lower())
+print(a.title())   #capitalize every word's first letter
+print(a.capitalize())   #capitalize only first letter
---
+++ salary.py
@@ -0,0 +1,8 @@
+salary=int(input("Enter your salary : "))
+medical_allowance_percent=int(input("Enter medical allowance percentage : "))
+gross_salary=salary+salary*medical_allowance_percent/100 
+print("Gross salary is" ,gross_salary)
+print("extra calculation")
+print(2+3*10/6)
+print(2+(3*(10/6)))
+print(10%6.0)
