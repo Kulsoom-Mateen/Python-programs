6e02198a12812a97b43b6f07fab083aa4f387943
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-03-27 14:02:44 -0400

    Added python program for table

---
+++ table.py
@@ -0,0 +1,7 @@
+'''for i in range(1,11):
+    print("3 * "+ str(i) + " = " + str(3*i))
+'''
+
+num=int(input("Enter a number : "))
+for i in range(1,11):
+    print(str(num) + "*" + str(i) + "=" + str(num*i))  #both statements are correct
