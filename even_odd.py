37011b5ca2246a62a46dab8785cd96cebb6058ef
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-03-27 14:08:11 -0400

    Added python program for even & odd numbers

---
+++ even_odd.py
@@ -0,0 +1,11 @@
+'''number=int(input("Enter any number : "))
+if number % 2 == 0:
+    print(str(number) +" is even number")
+else:
+    print(str(number) +" is odd number")'''
+
+num=int(input("Enter any number : "))
+if num%2==0:
+    print("Number is even")
+else:
+    print("Number is odd")
