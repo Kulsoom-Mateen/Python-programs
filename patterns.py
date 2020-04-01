dc8ba4e647dca5461f556beede6172bc8679f103
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-03-31 23:16:51 -0400

    Added python program for different patterns

---
+++ patterns.py
@@ -0,0 +1,21 @@
+for a in range(1,6):
+    for b in range(1,a+1):
+        print(b,"",end="")
+    print()        
+print("\n")            
+
+for l in range(1,6):
+        for m in range(1,l+1):
+                print(l,"",end="")
+        print("")
+print("\n")
+
+for i in range(1,6):
+        for j in range(1,i+1):
+                print("*","",end="")
+        print()
+print("\n")
+
+for j in range(1,7):
+        print('* '*j,end='')
+        print()
