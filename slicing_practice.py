bbb9a5bbc29cfd2ec580724b68d2196f453548e8
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-04-02 12:50:31 -0400

    Added python program for slicing practice

---
+++ slicing_practice.py
@@ -0,0 +1,17 @@
+lst=[1,5,6,78,34,22,444,7]
+new_lst=[]
+new_lst.append(lst[3:6])
+print(new_lst)
+print(lst)
+for i in range(4,7):
+    print(lst[i])
+
+list=[2,45,78,324,3465,23,545]
+list2=list
+print(list)
+print(list2)
+list2.append(45)
+print(list)
+print(list2)
+print(list[5:1:-1])
+print(list[7::-3])
