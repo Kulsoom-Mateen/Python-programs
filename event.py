19d45d2ba53fbbf3e6308767a8f7a5c5c5561e61
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-04-02 13:25:00 -0400

    Added python program for simple program practice

---
+++ event.py
@@ -0,0 +1,17 @@
+''' if return boolean either true or false // after if and else use indentation(tab)  //'''
+#Comaprison operators
+#  !=
+#  ==
+#  <=
+#  >=
+#  >
+#  <
+event_venue=input("Where is event : ")
+if event_venue=="Home" or event_venue=="home" or event_venue=="HOME" :
+    print("I am coming")
+elif event_venue==("Banquet") or event_venue=="banquet" or event_venue=="BANQUET":
+    print("Surely I will go")
+else:
+    print("I am busy, I cannot go!") 
+
+  
