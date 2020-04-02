33d76dd58faa0b5076afb4200a6b9c9e851a033f
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-04-02 14:11:44 -0400

    Added python program for zero division error

---
+++ division_by_zero_error.py
@@ -0,0 +1,7 @@
+try:
+    num1=int(input("enter 1st number : "))
+    num2=int(input("enter 2nd number : "))
+    div=num1/num2
+    print(num1,"/",num2,"=",div)
+except:
+    print("change value of denominator, 0 is not allowed")
