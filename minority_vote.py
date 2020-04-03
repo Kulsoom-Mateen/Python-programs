774f29f229fe45dc884e0f3bcff8f06c532e1147
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-04-03 18:46:43 -0400

    Added pytohn program for dinding least number of votes in a list

---
+++ minority_vote.py
@@ -0,0 +1,16 @@
+def minority_vote(vote1):
+    counter=0
+    least=0
+    for i in vote1:
+
+        count1=vote1.count(i)
+        if(count1>counter):
+            counter=count1
+            if(counter/len(vote1)>=0.5):
+                least=vote1[count1+1]
+            else:
+              
+                least=False
+    print(least)
+vote=["A","A","A","A","B","C","D","E"]
+minority_vote(vote)
