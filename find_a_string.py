2c4df97e97476f1c3f946e2207e2c707270f2a8d
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-04-15 23:49:58 -0400

    Added python program to find a string

---
+++ find_a_string.py
@@ -0,0 +1,23 @@
+def count_substring(string, sub_string):
+    s=0
+    for i in range(len(string)-len(sub_string)+1):
+        y=0
+        if(string[i]==sub_string[y]):
+            y=y+1
+            z=0
+            for j in range(i,i+len(sub_string)):
+                if(string[j]==sub_string[z]):
+                    f=True
+                else:
+                    f=False
+                    break
+                z=z+1
+            if(f==True):
+                s=s+1
+    return s
+
+string = "ABCDCD"
+sub_string = "CD"
+
+count = count_substring(string, sub_string)
+print("Number of occurence(s) of ", sub_string , " in " , string , " is : " ,count)
