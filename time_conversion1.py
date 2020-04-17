3109ead51fd1ca6dc213c790fdda77664024772c
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-04-17 02:33:58 -0400

    Added python program to convert time from 12 hour format to 24 hour format

---
+++ time_conversion1.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+def timeConversion(s):
+    time = s.split(":")
+    if s[-2:] == "PM":
+        if time[0] != "12":
+            time[0] = str(int(time[0])+12)
+    else:
+        if time[0] == "12":
+            time[0] = "00"
+    ntime = ':'.join(time)
+    return str(ntime[:-2])
+
+s = input("Enter time in 12 hour format i.e. 11:34:05AM  : ")
+result = timeConversion(s)
+print(result)
