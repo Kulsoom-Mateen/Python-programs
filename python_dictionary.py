1cacd54187ebde21b9b6ca27d385ddabd1c855ce
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-06-13 18:53:28 -0400

    Added python file for dictionary

--- python_dictionary.py
+++ python_dictionary.py
@@ -14,3 +14,15 @@ for a in d1.keys():
     print(a)
 for a,b in d1.items():
     print(a,":",b)
+if "A" in d1:
+    print("Yes")
+print(len(d1))
+d1["D"]="Doll"
+print(d1)
+d1.pop("D")
+print(d1)
+d1["D"]="doll"
+d1["E"]="Egg"
+print(d1)
+del d1["B"]
+print(d1)
