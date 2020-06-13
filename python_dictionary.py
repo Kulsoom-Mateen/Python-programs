78145204e6f9b1fc497b5a49252b68dbdf651889
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-06-13 19:01:45 -0400

    Added python file for dictionary

--- python_dictionary.py
+++ python_dictionary.py
@@ -26,3 +26,50 @@ d1["E"]="Egg"
 print(d1)
 del d1["B"]
 print(d1)
+d2 = d1.copy()
+print(d2)
+d3 = dict(d1)
+print(d3)
+
+####Nested dictionary
+family = {
+    "child1" : {
+        "name" : "Ali",
+        "age" : "10"
+    },
+    "child2" : {
+        "name" : "Amna",
+        "age" : "7"
+    },
+    "child3" : {
+        "name" : "Ahmed",
+        "age" : "3"
+    }
+}
+print(family)
+print(family["child1"])
+print(family["child2"]["name"])
+
+child1 = {
+        "name" : "Ali",
+        "age" : "10"
+}
+child2 = {
+        "name" : "Amna",
+        "age" : "7"
+}
+child3 = {
+        "name" : "Ahmed",
+        "age" : "3"
+    }
+myfamily = {
+    "child1" : child1,
+    "child2" : child2,
+    "child3" : child3
+}
+print(myfamily)
+print(myfamily["child3"])
+print(myfamily["child3"]["age"])
+
+d4 = dict(name="ABC",age="10")
+print(d4)
