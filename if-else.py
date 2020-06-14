259cae734069c991f8ba3c4ef2eb8bc5566c9a20
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-06-14 19:03:02 -0400

    Added python file for if-else use

--- if-else.py
+++ if-else.py
@@ -15,3 +15,15 @@ print("A") if a > b else print("B")
 c = 331
 d = 330
 print("C") if c > d else print("=") if c == d else print("D")
+
+e = 200
+f = 33
+g = 500
+if e > f and g > e:
+    print("Both conditions are True")
+
+h = 200
+i = 33
+j = 500
+if h > i or h > j:
+    print("At least one of the conditions is True")
