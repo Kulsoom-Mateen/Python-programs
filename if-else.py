51463745c33a645b1d1b6171a4bf6412061a2022
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-06-14 19:01:09 -0400

    Added python file for if-else use

---
+++ if-else.py
@@ -0,0 +1,17 @@
+if 10>5:
+    print("10 is greater than 5")
+elif 10<5:
+    print("10 is less than 5")
+else:
+    print("HH")
+
+#Short hand if else
+
+if 5<10: print("5 is less than 10")
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+c = 331
+d = 330
+print("C") if c > d else print("=") if c == d else print("D")
