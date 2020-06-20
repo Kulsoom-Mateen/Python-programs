d82c51f43b012bbf8d4f6da2357eba8cf0de60e9
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-06-19 22:49:05 -0400

    Added python file for for loop

--- for_loops.py
+++ for_loops.py
@@ -29,3 +29,15 @@ for x in range(2, 6):
 
 for x in range(2, 30, 3):
     print(x)
+
+for x in range(6):
+    print(x)
+else:
+    print("Finally finished!")
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
