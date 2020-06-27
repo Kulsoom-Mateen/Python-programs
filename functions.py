0dd543de622a966a8916367aba5a5e80b89595b8
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-06-26 23:10:33 -0400

    Added pyhton file for functions

--- functions.py
+++ functions.py
@@ -7,6 +7,19 @@ def my_function1(fname):
 def my_function2(fname, lname):
   print(fname + " " + lname)
 
+def my_function3(*kids):
+  print("The youngest child is " + kids[2])
+
+def my_function4(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+def my_function5(**kid):
+  print("His last name is " + kid["lname"])
+
+def my_function6(country = "Norway"):
+  print("I am from " + country)
+
+
 my_function()
 
 my_function1("Emil")
@@ -15,4 +28,13 @@ my_function1("Linus")
 
 my_function2("Emil", "Refsnes")
 
+my_function3("Emil", "Tobias", "Linus")
 
+my_function4(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+my_function5(fname = "Tobias", lname = "Refsnes")
+
+my_function6("Pakistan")
+my_function6("India")
+my_function6()
+my_function6("Brazil")
